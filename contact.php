fe6ea36d327c2c015e570ec26f6399658f3e3811
Srivatsan Viswanathan <sriv.viswanathan@gmail.com>
2022-05-07 15:09:14 -0400

    form

--- contact.php
+++ contact.php
@@ -16,15 +16,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["firstname"]) != '') {
  $email_body .= "Email: " . $email_address;
  $email_body .= "Message: " . $message;
 
- $email_from = "From: " . $email_to;
+ $email_from = "From: " . $email_address;
+
+ ini_set("SMTP", "aspmx.l.google.com");
+ ini_set("sendmail_from", $email_address);
   
  mail($email_to, $email_subject, $email_body, $email_from);
 
- if (mail($email_to, $email_subject, $email_body, $email_from)) {
-  echo "Message accepted";
- } else {
-  echo "Error: Message not accepted";
- }
 }
 
 ?>
