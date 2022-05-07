decd3e3fd0e04640035ee74f71f1df691ce9d8e8
Srivatsan Viswanathan <sriv.viswanathan@gmail.com>
2022-05-07 13:51:04 -0400

    form

--- contact.php
+++ contact.php
@@ -1,21 +1,26 @@
 <?php
+if ($_POST["firstname"] != '' && $_POST["lastname"] != '' && $_POST["email"] != '' 
+&& $_POST["subject"] != '' && $_POST["message"] != '') {
 
-$to = "sriv.viswanathan@gmail.com";
+ $to = "sriv.viswanathan@gmail.com";
 
-$firstname = $_POST["firstname"];
-$lastname = $_POST["lastname"];
-$email_address = $_POST["email"];
-$subject = $_POST["subject"];
-$message = $_POST["message"];
+ $firstname = $_POST["firstname"];
+ $lastname = $_POST["lastname"];
+ $email_address = $_POST["email"];
+ $subject = $_POST["subject"];
+ $message = $_POST["message"];
 
-$email_subject = $subject;
+ $email_subject = $subject;
 
-$email_body = "From: ".$firstname." ".$lastname."\r\n";
-$email_body .= "Email: ".$email_address."\r\n";
-$email_body .= "Message: ".$message."\r\n";
 
-$email_header = "From: ".$email_address;
+ $email_body = "From: " . $firstname . " " . $lastname . "\n";
+ $email_body .= "Email: " . $email_address . "\n";
+ $email_body .= "Message: " . $message . "\n";
 
-mail($to, $email_subject, $email_body, $email_header);
+ $email_header = "From: " . $email_address . "\r\n";
+
+ mail($to, $email_subject, $email_body, $email_header);
+
+}
 
 ?>
