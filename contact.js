05a54e5439465d3755a637e56ba0f7da66c83857
Srivatsan Viswanathan <sriv.viswanathan@gmail.com>
2023-03-15 22:21:53 -0400

    Form update

--- contact.js
+++ contact.js
@@ -1,75 +1,77 @@
 function validate() {
- passed = false;
- const firstname = document.getElementById("firstname").value;
- const lastname = document.getElementById("lastname").value;
- var hasNumber = /\d/;
+  passed = false;
+  const firstname = document.getElementById("firstname").value;
+  const lastname = document.getElementById("lastname").value;
+  var hasNumber = /\d/;
 
- if (hasNumber.test(firstname) || hasNumber.test(lastname)) {
+  if (hasNumber.test(firstname) || hasNumber.test(lastname)) {
+    if (hasNumber.test(firstname)) {
+      document.getElementsByClassName("errorMessage1")[0].style.display =
+        "block";
+      document.getElementsByClassName("errorMessage1")[0].textContent =
+        "Error. Numbers not allowed.";
+    }
+    if (hasNumber.test(lastname)) {
+      document.getElementsByClassName("errorMessage2")[0].style.display =
+        "block";
+      document.getElementsByClassName("errorMessage2")[0].textContent =
+        "Error. Numbers not allowed.";
+    }
+    if (!hasNumber.test(firstname)) {
+      document.getElementsByClassName("errorMessage1")[0].style.display =
+        "none";
+      document.getElementsByClassName("errorMessage1")[0].textContent = "";
+    }
+    if (!hasNumber.test(lastname)) {
+      document.getElementsByClassName("errorMessage2")[0].style.display =
+        "none";
+      document.getElementsByClassName("errorMessage2")[0].textContent = "";
+    }
+  } else {
+    passed = true;
+  }
 
-  if (hasNumber.test(firstname)) {
-   document.getElementsByClassName("errorMessage1")[0].style.display = "block";
-   document.getElementsByClassName("errorMessage1")[0].textContent = "Error. Numbers not allowed.";
+  if (passed == true) {
+    sendEmail();
+    document.getElementById("contact-form").reset();
+    document.getElementsByClassName("errorMessage1")[0].style.display = "none";
+    document.getElementsByClassName("errorMessage1")[0].textContent = "";
+    document.getElementsByClassName("errorMessage2")[0].style.display = "none";
+    document.getElementsByClassName("errorMessage2")[0].textContent = "";
   }
-  if (hasNumber.test(lastname)) {
-   document.getElementsByClassName("errorMessage2")[0].style.display = "block";
-   document.getElementsByClassName("errorMessage2")[0].textContent = "Error. Numbers not allowed.";
-  }
-  if (!hasNumber.test(firstname)) {
-   document.getElementsByClassName("errorMessage1")[0].style.display = "none";
-   document.getElementsByClassName("errorMessage1")[0].textContent = "";
-  }
-  if (!hasNumber.test(lastname)) {
-   document.getElementsByClassName("errorMessage2")[0].style.display = "none";
-   document.getElementsByClassName("errorMessage2")[0].textContent = "";
-  }
- }
- else {
-  passed = true;
- }
-
- if (passed == true) {
-  sendEmail();
-  document.getElementById("contact-form").reset();
-  document.getElementsByClassName("errorMessage1")[0].style.display = "none";
-  document.getElementsByClassName("errorMessage1")[0].textContent = "";
-  document.getElementsByClassName("errorMessage2")[0].style.display = "none";
-  document.getElementsByClassName("errorMessage2")[0].textContent = "";
- }
 }
 
-
 function sendEmail() {
- const firstname = document.getElementById("firstname").value;
- const lastname = document.getElementById("lastname").value;
- const username = firstname + " " + lastname;
- const email = document.getElementById("email").value;
- const subject = document.getElementById("subject").value;
- const message = document.getElementById("message").value;
+  const firstname = document.getElementById("firstname").value;
+  const lastname = document.getElementById("lastname").value;
+  const username = firstname + " " + lastname;
+  const email = document.getElementById("email").value;
+  const subject = document.getElementById("subject").value;
+  const message = document.getElementById("message").value;
 
- fetch("https://formsubmit.co/ajax/5c593b081c27f6f905dae6e9b448a925", {
-  method: "POST",
-  headers: {
-   'Content-Type': 'application/json',
-   'Accept': 'application/json'
-  },
-  body: JSON.stringify({
-   name: username,
-   email: email,
-   subject: subject,
-   message: message
-  })
- })
+  fetch("https://formsubmit.co/el/zahoho", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: username,
+      email: email,
+      subject: subject,
+      message: message,
+    }),
+  });
 
- confirmation();
+  confirmation();
 }
 
 function confirmation() {
- const confirm = document.getElementById("confirm");
- confirm.textContent = "Email has been sent. Thank you.";
- confirm.setAttribute("class", "confirm");
+  const confirm = document.getElementById("confirm");
+  confirm.textContent = "Email has been sent. Thank you.";
+  confirm.setAttribute("class", "confirm");
 
- setTimeout(function () {
-  confirm.classList.add("confirm-end");
- }, 14900);
-
+  setTimeout(function () {
+    confirm.classList.add("confirm-end");
+  }, 14900);
 }
