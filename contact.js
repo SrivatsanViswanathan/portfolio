84c1606ff5e357a6ae53cf6a563034b158bcdc34
Srivatsan Viswanathan <sriv.viswanathan@gmail.com>
2023-03-15 22:29:43 -0400

    Form Update

--- contact.js
+++ contact.js
@@ -49,7 +49,7 @@ function sendEmail() {
   const subject = document.getElementById("subject").value;
   const message = document.getElementById("message").value;
 
-  fetch("https://formsubmit.co/el/zahoho", {
+  fetch("https://formsubmit.co/ajax/92a64e78aa951ec6fa59c2d6643994f5", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
